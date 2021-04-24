7a75c2337c6295ef5ace36e6df184958c7c62155
polychemky@163.com <constant@YuandeMacBook-Pro.local>
2021-04-24 23:14:54 +0800

    delete target

--- ioc-container-overview/src/main/java/org/example/thinking/in/spring/ioc/overview/enums/City.java
+++
@@ -1,7 +0,0 @@
-package org.example.thinking.in.spring.ioc.overview.enums;
-
-public enum City {
-    SHENZHENG,
-    BEIJING,
-    GUANGZHOU
-}
--- spring-bean/src/main/java/org/example/thinking/in/spring/bean/factory/UserFactory.java
+++
@@ -1,9 +0,0 @@
-package org.example.thinking.in.spring.bean.factory;
-
-import org.example.thinking.in.spring.ioc.overview.domain.User;
-
-public interface UserFactory {
-    default User createUser(){
-        return User.createUser();
-    };
-}
