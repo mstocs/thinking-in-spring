8355cff6363343c591af489a781f7f54b9285f4d
Constant <constant@iMac.local>
2021-07-17 00:00:25 +0800

    finish course 96, InstantiationAwareBeanPostProcessor#postProcessAfterInstantiation + postProcessProperties

--- bean-lilfecycle/src/main/java/org/example/thinking/in/spring/bean/lificycle/BeanInstantiationLIfeCycleDemo.java
+++ bean-lilfecycle/src/main/java/org/example/thinking/in/spring/bean/lificycle/BeanInstantiationLIfeCycleDemo.java
@@ -3,6 +3,8 @@ package org.example.thinking.in.spring.bean.lificycle;
 import org.example.thinking.in.spring.ioc.overview.domain.SuperUser;
 import org.example.thinking.in.spring.ioc.overview.domain.User;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -44,6 +46,37 @@ public class BeanInstantiationLIfeCycleDemo {
             if(ObjectUtils.nullSafeEquals("superUser", beanName) && SuperUser.class.equals(beanClass)) {
                 //把配置完成的superUser bean覆盖
                 return new SuperUser();
+            }
+            return null;
+        }
+
+        @Override
+        public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+            if (ObjectUtils.nullSafeEquals("user", beanName) && User.class.equals(bean.getClass())) {
+                User user = (User) bean;
+                user.setId(2L);
+                user.setName("mercyBlitz");
+                //跳过bean的populate过程
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+            if(ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
+                final MutablePropertyValues propertyValues;
+                if(pvs instanceof MutablePropertyValues) {
+                    propertyValues = (MutablePropertyValues) pvs;
+                } else {
+                    propertyValues = new MutablePropertyValues();
+                }
+                propertyValues.addPropertyValue("number", "2");
+                if(propertyValues.contains("description")) {
+                    propertyValues.removePropertyValue("description");
+                    propertyValues.addPropertyValue("description", "this is user Holder v2");
+                }
+                return propertyValues;
             }
             return null;
         }
--- bean-lilfecycle/src/main/java/org/example/thinking/in/spring/bean/lificycle/UserHolder.java
+++ bean-lilfecycle/src/main/java/org/example/thinking/in/spring/bean/lificycle/UserHolder.java
@@ -10,14 +10,37 @@ import org.example.thinking.in.spring.ioc.overview.domain.User;
 public class UserHolder {
     private final User user;
 
-    public UserHolder(User user) {
-        this.user = user;
-    }
+    private Integer number;
+
+    private String description;
 
     @Override
     public String toString() {
         return "UserHolder{" +
                 "user=" + user +
+                ", number=" + number +
+                ", description='" + description + '\'' +
                 '}';
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public UserHolder(User user) {
+        this.user = user;
+    }
+
 }
